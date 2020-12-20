cf649962732cf6460e7376ebe792940315429b58
Matheus Machado <matheus_lm10@hotmail.com>
2020-12-20 18:10:59 -0400

    Integrates with redux devtools extension

--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
 import { createStore } from "redux";
 
 import { reducers } from "./reducers";
+import { devToolsEnhancer } from "redux-devtools-extension";
 
-const store = createStore(reducers);
+const store = createStore(reducers, devToolsEnhancer());
 
 export { store };
