e177d33f95fcc5e2dd01ec817a06e3c1a4fe290a
Matheus Machado <matheus_lm10@hotmail.com>
2020-12-23 21:40:29 -0400

    refactor: replaces css styles for styled components on the App component.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 
 import NotesMaker from "../components/notes-maker";
 import NotesContainer from "../components/notes-container";
-import "./style.css";
+
+const StyledApp = styled.div`
+  display: grid;
+  background-color: var(--background-color);
+`;
+
+const Title = styled.h2`
+  color: var(--primary-text-color);
+  margin: 10px 0;
+  text-align: center;
+  text-shadow: 3px 3px 4px rgba(0, 0, 0, 1);
+`;
 
 const App = () => (
-  <div className="app">
-    <h2 className="app-title">Notes App</h2>
+  <StyledApp>
+    <Title>Notes App</Title>
     <NotesMaker />
     <NotesContainer />
-  </div>
+  </StyledApp>
 );
 
 export default App;
