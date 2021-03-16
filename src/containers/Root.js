d771bc12d49b095189557a58b8dfbc29da940711
Matheus Lima Machado <matmachado@uolinc.com>
2021-03-16 08:22:55 -0400

    refactor: changes the way of reading auth0 credentials

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -4,12 +4,11 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 import App from "./App";
 import { store } from "../store";
-import authConfig from "../auth-config.json";
 
 const Root = () => (
   <Auth0Provider
-    domain={authConfig.auth0Domain}
-    clientId={authConfig.auth0ClientID}
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
     redirectUri={window.location.origin}
   >
     <Provider store={store}>
