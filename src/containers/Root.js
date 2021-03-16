920e3eeae76a3c66c196efcb528bd417b37396b0
Matheus Lima Machado <matmachado@uolinc.com>
2021-03-16 08:48:39 -0400

    fix: changes the origin url

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -9,7 +9,7 @@ const Root = () => (
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH0_DOMAIN}
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-    redirectUri={window.location.origin}
+    redirectUri={process.env.REACT_APP_ORIGIN_URL}
   >
     <Provider store={store}>
       <App />
